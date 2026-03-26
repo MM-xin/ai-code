1ce71c303b295e89bb8a20f9c8a9226efcf08580
yy <11252180+yyshui@user.noreply.gitee.com>
2026-03-26 16:44:21 +0800

    前端添加全局拦截器
    Signed-off-by: yy <11252180+yyshui@user.noreply.gitee.com>

--- ai-code-frontend/src/App.vue
+++ ai-code-frontend/src/App.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import BasicLayout from '@/layouts/BasicLayout.vue'
+
+import { sayHello } from '@/api/apiController.ts'
+
+sayHello().then((res) => {
+  console.log(res)
+})
 </script>
 
 <template>
